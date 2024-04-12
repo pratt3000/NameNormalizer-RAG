4b33e3bf150649cc2ee9da7be715616bae583fe0
pratt3000 <pratt3000@gmail.com>
2024-04-12 12:30:16 -0500

    need to add qdrant code next

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 import random
 import uvicorn
 
+from src.main import get_main_entity
+
 app = FastAPI()
 
-@app.get("/patternMatcher/{query}")
-async def root(query: str = None):
-    return {"message": f"Hello World {random.randint(1, 100)}", "query": query}
+@app.get("/pm/{query}")
+async def get_merchant(query: str = None):
+    merchant_name = get_main_entity(query)
+    return {"query": query, "result": merchant_name}
 
 @app.get("/")
 async def root(query: str = None):
-    return {"message": f"Hello World"}
+    return {"message": f"Hello World {random.randint(1, 100)}"}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8888)
---
+++ src/main.py
@@ -0,0 +1,4 @@
+
+
+def get_main_entity(query: str):
+    return 'entity detection not built yet'
